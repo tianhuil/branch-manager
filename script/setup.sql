b6e577edb896e4514bb2cb2e3ef173409dc75426
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2025-10-07 12:22:13 -0400

    Enhance database setup script by adding connection termination command before dropping the database and user. This ensures a clean setup process by terminating existing connections to the target database.

--- script/setup.sql
+++ script/setup.sql
@@ -2,8 +2,11 @@
 -- Example usage: psql -v db_name='mydb' -v db_user='myuser' -v db_password='mypassword' -f setup.sql
 \set HISTCONTROL ignorespace
 
--- Revoke all privileges from user on the database (if exists)
-REVOKE ALL PRIVILEGES ON DATABASE :db_name FROM :db_user;
+-- Terminate all connections to the target database (except this one)
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = :'db_name'
+  AND pid <> pg_backend_pid();
 
 -- Drop database and user if they exist
 DROP DATABASE IF EXISTS :db_name;
