730addf79544857c8fd848dacf5d1b5b83334e06
Cauã Rinaldi <cauarinaldi451@gmail.com>
2025-10-17 15:27:29 -0300

    Prettier

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -6,8 +6,10 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: (() => {
     if (process.env.DATABASE_URL) {
-      return { url: process.env.DATABASE_URL }
+      return { url: process.env.DATABASE_URL };
     }
-    console.warn("DATABASE_URL is not set. You won't be able to run migrate, push and pull commands.");
+    console.warn(
+      "DATABASE_URL is not set. You won't be able to run migrate, push and pull commands."
+    );
   })(),
 });
