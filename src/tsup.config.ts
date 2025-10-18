daf3665c0d5436c8e8c6768ceb8a8c4b3e2ce4f1
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2025-10-17 21:36:35 -0400

    Refactor package.json and tsconfig.json for improved configuration
    - Removed type declarations from the "exports" field in package.json to streamline the module exports.
    - Disabled composite builds in tsconfig.json to simplify the TypeScript configuration.
    - Updated tsup.config.ts to disable TypeScript declaration file generation, optimizing the build process.

--- src/tsup.config.ts
+++ src/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["index.ts", "db.ts", "preview.ts", "logger.ts", "util.ts"],
   format: ["esm"],
-  dts: true,
+  dts: false,
   sourcemap: true,
   clean: true,
   outDir: "../dist",
